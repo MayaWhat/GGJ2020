820bd89c43e52fe62efe5b5239c9ebe5f6088d68
Zachary Billington <abc.zacbill@gmail.com>
2020-02-01 00:13:07 +1300

    player and enemey

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+public abstract class Card : MonoBehaviour
 {
     [SerializeField]
     protected int _cost;
