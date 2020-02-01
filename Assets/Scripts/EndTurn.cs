b581937320443016aa48d5c8bfd70b7a91e49418
Zachary Billington <abc.zacbill@gmail.com>
2020-02-01 23:47:17 +1300

    End turn button

--- Assets/Scripts/EndTurn.cs
+++ Assets/Scripts/EndTurn.cs
@@ -8,24 +8,33 @@ public class EndTurn : MonoBehaviour
     private GameManager _gameManager;
     private Button _button;
     private Hand _hand;
+    private Sprite _youMayEndTurnSprite;
+    private Sprite _youCantEndTurnSprite;
+    private Sprite _youCanOnlyEndTurnSprite;
+    private Image _spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
+        _spriteRenderer = GetComponent<Image>();
         _hand = FindObjectOfType<Hand>();
         _button = GetComponent<Button>();
+        _youMayEndTurnSprite = Resources.Load<Sprite>("Sprites/End Turn Button/endturn_but_enabled");
+        _youCantEndTurnSprite = Resources.Load<Sprite>("Sprites/End Turn Button/endturn_but_disabled");
+        _youCanOnlyEndTurnSprite = Resources.Load<Sprite>("Sprites/End Turn Button/endturn_but_enabled_hover");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!_hand.CanPlaySomething() && !Player.Instance.IsDead && !Enemy.Instance.IsDead) {
-            GetComponent<Image>().color = Color.blue; 
+        if (!_hand.CanPlaySomething() && !Player.Instance.IsDead && !Enemy.Instance.IsDead) 
+        {
+            _spriteRenderer.sprite = _youCanOnlyEndTurnSprite;
         }
         else 
         {
-            GetComponent<Image>().color = Color.white; 
+            _spriteRenderer.sprite = _youMayEndTurnSprite;
         }
     }
 
