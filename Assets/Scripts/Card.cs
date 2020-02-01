9c494bc4a6bed32f19100145b1d3692eda4a1255
MyselfWhat <ian@devolutious.co.nz>
2020-02-02 11:13:12 +1300

    Card sounds

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -136,6 +136,8 @@ public abstract class Card : MonoBehaviour
 
     private IEnumerator AnimatePlay()
     {
+        GameManager.Instance.Sounds.CastSpell.Play();
+
         var position = transform.position;
 
         transform.SetParent(_canvas.transform);
