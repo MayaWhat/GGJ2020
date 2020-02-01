07b5ef7ecbc1dc43d83b5d3a19fec4b85e849740
Ben Stewart <ben.stewart@enlighten.co.nz>
2020-02-02 11:18:23 +1300

    You can now click split cards again

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -193,6 +193,8 @@ public abstract class Card : MonoBehaviour
             GameObject.Destroy(transform.gameObject);
             GameObject.Destroy(_splitParticles);
             GameManager.Instance.Busyness--;
+            halves.LeftHalf.Clickable = true;
+            halves.RightHalf.Clickable = true;
         });
     }
 
@@ -247,7 +249,7 @@ public abstract class Card : MonoBehaviour
         return (leftHalf, rightHalf);
     }
 
-    public enum CardSymbol 
+    public enum CardSymbol
     {
         Attack = 0,
         Block = 1
