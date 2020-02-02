fe42652d16c8af6463a0c7d6d19db99775893cd0
Zachary Billington <abc.zacbill@gmail.com>
2020-02-02 16:14:48 +1300

    Combine cards no longer cost double

--- Assets/Scripts/CombinedCard.cs
+++ Assets/Scripts/CombinedCard.cs
@@ -9,8 +9,6 @@ public class CombinedCard : Card
     }
 
     public void ApplyEffects() {
-        _playerEnergy.Energy -= _cost;
-
         if (_cardSymbol == CardSymbol.Attack) {
             GameManager.Instance.Enemy.TakeDamage(_value);
         } 
